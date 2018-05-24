9c6d596329d6978092113dcfecc51e93d262a6d2
BRIAN ZHANG <brian.zhang@brianzhang-ltm.local>
2018-05-23 22:31:19 -0700

    Initial Commit

---
+++ Fitness Tracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fitness Tracker
+//
+//  Created by BRIAN ZHANG on 5/23/18.
+//  Copyright © 2018 Brian Zhang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
