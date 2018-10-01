6d0ccc718528abc92d3420bd8a551ce74629167e
brian-zhang98 <brian.zhang98@gmail.com>
2018-10-01 14:43:42 -0700

    added basic gym item list functionality

--- fitness-tracker/MyGymItemsCell.swift
+++ fitness-tracker/MyGymItemsCell.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class MyGymItemsCell: UICollectionViewCell {
     
+    @IBOutlet weak var gymItem: UITextField!
+    @IBOutlet weak var gymCheckbox: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- fitness-tracker/view-controllers/GymViewController.swift
+++ fitness-tracker/view-controllers/GymViewController.swift
@@ -8,8 +8,32 @@
 
 import UIKit
 
-class GymViewController: UIViewController {
+class GymViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    struct data {
+        var text:String
+        var count:String
+    }
+    
+    let reuseIdentifier = "gymListCell"
+    var gymItemsData: [data] = []
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //return gymItemsData.count
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyGymItemsCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        //cell.textLabel.text = self.caloriesData[indexPath.item].text
+        //cell.countLabel.text = self.caloriesData[indexPath.item].count
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
