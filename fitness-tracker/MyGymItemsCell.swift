dd46010082cb707ced262b8f47d5e2dfe62a9d53
brian-zhang98 <brian.zhang98@gmail.com>
2018-10-01 12:41:18 -0700

    progress in gym view controller

---
+++ fitness-tracker/MyGymItemsCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyGymItemsCell.swift
+//  fitness-tracker
+//
+//  Created by BRIAN ZHANG on 9/30/18.
+//  Copyright © 2018 Brian Zhang. All rights reserved.
+//
+
+import UIKit
+
+class MyGymItemsCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
